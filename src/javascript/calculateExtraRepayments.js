41c757dc8f890ff98c2da98093bbe3a40711d6ea
Rowan <volkwyn.rowan@gmail.com>
2024-05-11 14:02:29 +1000

    implemented extra repayment calculator

--- src/javascript/calculateExtraRepayments.js
+++ src/javascript/calculateExtraRepayments.js
@@ -9,48 +9,71 @@ export function calculateExtraRepayments() {
     let effectiveInterestRate;
     let numRepayments;
 
-    let numRepaymentsExtra;
-    let monthlyPayment;
-
     const amount = document.getElementById('amount');
 
-    if (repaymentFrequency == 'weekly') {
-        // Original Repayment
-        effectiveInterestRate = interestRate / 5200;
-        numRepayments = loanTerm * 52;
-        calculatedRepayment = Math.ceil(loanAmount * ((effectiveInterestRate*Math.pow(1+effectiveInterestRate, numRepayments)) / 
-                                                        (Math.pow(1 + effectiveInterestRate, numRepayments) - 1)));
-        amount.innerHTML = `Repayment: $${calculatedRepayment} per week`;
+    if (loanAmount && loanTerm && interestRate && extraPayment) {
+        if (repaymentFrequency == 'weekly') {
+            // Original Repayment
+            effectiveInterestRate = interestRate / 5200;
+            numRepayments = loanTerm * 52;
+            calculatedRepayment = Math.ceil(loanAmount * ((effectiveInterestRate*Math.pow(1+effectiveInterestRate, numRepayments)) / 
+                                                            (Math.pow(1 + effectiveInterestRate, numRepayments) - 1)));
+    
+            // Extra Repayments
+            const totalPayment = Number(calculatedRepayment) + Number(extraPayment);
+    
+            // Perform Calculations
+            const numerator = Math.log(((totalPayment) / (totalPayment - (loanAmount * effectiveInterestRate))))
+            const denominator = Math.log(1 + effectiveInterestRate);
+            const years = Math.floor((numerator/denominator) / 52);
+            const weeks = Math.round((numerator/denominator) % 52);
+    
+            // Update the DOM
+            amount.innerHTML = `By paying $${extraPayment} extra per week, your loan will be repayed in ${years} 
+            years and ${weeks} weeks instead of ${loanTerm} years.`;
+    
+        } else if (repaymentFrequency == 'fortnightly') {
+           // Original Repayment
+           effectiveInterestRate = interestRate / 2600;
+           numRepayments = loanTerm * 26;
+           calculatedRepayment = Math.ceil(loanAmount * ((effectiveInterestRate*Math.pow(1+effectiveInterestRate, numRepayments)) / 
+                                                           (Math.pow(1 + effectiveInterestRate, numRepayments) - 1)));
+    
+           // Extra Repayments
+           const totalPayment = Number(calculatedRepayment) + Number(extraPayment);
+    
+           // Perform Calculations
+           const numerator = Math.log(((totalPayment) / (totalPayment - (loanAmount * effectiveInterestRate))))
+           const denominator = Math.log(1 + effectiveInterestRate);
+           const years = Math.floor((numerator/denominator) / 26);
+           const weeks = 2 * Math.round((numerator/denominator) % 26);
+    
+           // Update the DOM
+           amount.innerHTML = `By paying $${extraPayment} extra per week, your loan will be repayed in ${years} 
+           years and ${weeks} weeks instead of ${loanTerm} years.`;
+        } else if (repaymentFrequency == 'monthly') {
+           // Original Repayment
+           effectiveInterestRate = interestRate / 1200;
+           numRepayments = loanTerm * 12;
+           calculatedRepayment = Math.ceil(loanAmount * ((effectiveInterestRate*Math.pow(1+effectiveInterestRate, numRepayments)) / 
+                                                           (Math.pow(1 + effectiveInterestRate, numRepayments) - 1)));
+    
+           // Extra Repayments
+           const totalPayment = Number(calculatedRepayment) + Number(extraPayment);
+    
+           // Perform Calculations
+           const numerator = Math.log(((totalPayment) / (totalPayment - (loanAmount * effectiveInterestRate))))
+           const denominator = Math.log(1 + effectiveInterestRate);
+           const years = Math.floor((numerator/denominator) / 12);
+           const months = Math.round((numerator/denominator) % 12);
+    
+           // Update the DOM
+           amount.innerHTML = `By paying $${extraPayment} extra per week, your loan will be repayed in ${years} 
+           years and ${months} months instead of ${loanTerm} years.`;
+        } else {
+            amount.innerHTML = 'Please choose a valid repayment frequency.';
+        };
+    }
 
-
-        // Extra Repayments
-        const totalPayment = Number(calculatedRepayment) + Number(extraPayment);
-
-        const numerator = Math.log(((totalPayment) / (loanAmount * effectiveInterestRate)) + 1)
-        const denominator = Math.log(1 + effectiveInterestRate);
-        console.log(calculatedRepayment);
-        console.log(extraPayment);
-        console.log(totalPayment);
-        console.log(effectiveInterestRate);
-        console.log(`numerator: ${numerator}`);
-        console.log(`denominator: ${denominator}`);
-        console.log(`weeks: ${numerator/denominator}`);
-
-
-
-    } else if (repaymentFrequency == 'fortnightly') {
-        effectiveInterestRate = interestRate / 2600;
-        numRepayments = loanTerm * 26;
-        calculatedRepayment = Math.ceil(loanAmount * ((effectiveInterestRate*Math.pow(1+effectiveInterestRate, numRepayments)) / 
-                                                        (Math.pow(1 + effectiveInterestRate, numRepayments) - 1)));
-        amount.innerHTML = `Repayment: $${calculatedRepayment} per fortnight`;
-    } else if (repaymentFrequency == 'monthly') {
-        effectiveInterestRate = interestRate / 1200;
-        numRepayments = loanTerm * 12;
-        calculatedRepayment = Math.ceil(loanAmount * ((effectiveInterestRate*Math.pow(1+effectiveInterestRate, numRepayments)) / 
-                                                        (Math.pow(1 + effectiveInterestRate, numRepayments) - 1)));
-        amount.innerHTML = `Repayment: $${calculatedRepayment} per month`;
-    } else {
-        amount.innerHTML = 'Please choose a valid repayment frequency.';
-    };
+    
 };
